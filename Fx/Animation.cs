94e6fd385e1922f76159228e29aac5e3a28cc549
yishn <shen.yichuan@gmail.com>
2014-11-27 18:58:35 +0100

    Add Animation class

---
+++ Fx/Animation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Trail.Fx {
+    public class Animation : Timer {
+        private int timeCounter;
+
+        public int Duration { get; private set; }
+        public Func<double, double> AnimationFunction { get; set; }
+
+        public new event EventHandler<AnimationValueEventArgs<double>> Tick;
+        public event EventHandler Complete;
+
+        public Animation() : this(15) { }
+        public Animation(int duration) {
+            this.AnimationFunction = (t) => Math.Pow(Math.Sin(Math.PI / 2 * t), 3);
+
+            if (duration <= 0) throw new ArgumentException();
+            this.Duration = duration;
+
+            this.Enabled = false;
+            this.Interval = 1;
+            base.Tick += new EventHandler(Animation_Tick);
+        }
+
+        private void Animation_Tick(object sender, EventArgs e) {
+            if (Duration == 0) {
+                Complete(this, new EventArgs());
+                return;
+            }
+
+            double time = 1.0 * timeCounter / Duration;
+            double value = AnimationFunction(time);
+            if (Tick != null) Tick(this, new AnimationValueEventArgs<double>(value));
+
+            if (timeCounter == Duration) {
+                this.Enabled = false;
+                if (Complete != null) Complete(this, new EventArgs());
+
+                return;
+            }
+
+            timeCounter++;
+        }
+
+        public new Animation Start() {
+            if (this.Enabled) return this;
+            this.timeCounter = 0;
+            this.Enabled = true;
+
+            return this;
+        }
+
+        public new Animation Stop() {
+            this.Enabled = false;
+            return this;
+        }
+    }
+}
---
+++ Fx/IntAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trail.Fx {
+    public class IntAnimation : Animation {
+        private int start, end;
+
+        public new event EventHandler<AnimationValueEventArgs<int>> Tick;
+
+        public IntAnimation() {
+            base.Tick += IntAnimation_Tick;
+        }
+
+        private void IntAnimation_Tick(object sender, AnimationValueEventArgs<double> e) {
+            int value = (int)(e.Value * (end - start));
+            if (Tick != null) Tick(this, new AnimationValueEventArgs<int>(value));
+        }
+
+        public new IntAnimation Start() { return this.Start(0, 100); }
+        public IntAnimation Start(int start, int end) {
+            this.start = start;
+            this.end = end;
+
+            return this;
+        }
+    }
+}
