acc1ae03892db044af6fe3d70af13ff08565bb6c
yishn <shen.yichuan@gmail.com>
2014-11-28 21:41:14 +0100

    Add ColumnItem class

---
+++ Controls/ColumnItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trail.Controls {
+    public class ColumnItem : ListViewItem {
+        public ItemsColumn SubColumn { get; set; }
+    }
+}
