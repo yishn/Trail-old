8187c9690819f7f2c87c1dad76847aa0fe2075e3
yishn <shen.yichuan@gmail.com>
2014-12-11 22:06:46 +0100

    Add StringHelper class

---
+++ Helpers/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Trail.Helpers {
+    public static class StringHelper {
+        public static bool MatchesPattern(this string input, string pattern) {
+            string regex = "^" + Regex.Escape(pattern).Replace(@"\*", ".*").Replace(@"\?", ".") + "$";
+            return Regex.IsMatch(input, regex, RegexOptions.IgnoreCase);
+        }
+    }
+}
