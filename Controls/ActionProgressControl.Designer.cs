2b9b0c14b0d5067d09f172806592d020d5aa03f5
yishn <shen.yichuan@gmail.com>
2014-12-23 21:44:05 +0100

    Add ActionProgressControl

---
+++ Controls/ActionProgressControl.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace Trail.Controls {
+    partial class ActionProgressControl {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // ActionProgressControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(28)))));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
+            this.Name = "ActionProgressControl";
+            this.Size = new System.Drawing.Size(200, 44);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
