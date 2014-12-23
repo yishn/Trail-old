687c8c4855d5567a55277ce43774d8649c2572ce
yishn <shen.yichuan@gmail.com>
2014-12-23 23:15:06 +0100

    Add HeaderText & DescriptionText

--- Controls/ActionProgressControl.Designer.cs
+++ Controls/ActionProgressControl.Designer.cs
@@ -25,6 +25,8 @@
         private void InitializeComponent() {
             this.progressBar = new System.Windows.Forms.Panel();
             this.progressBarValue = new System.Windows.Forms.Panel();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
             this.progressBar.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,12 +48,33 @@
             this.progressBarValue.Size = new System.Drawing.Size(150, 5);
             this.progressBarValue.TabIndex = 0;
             // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoEllipsis = true;
+            this.headerLabel.Location = new System.Drawing.Point(3, 3);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(194, 15);
+            this.headerLabel.TabIndex = 1;
+            this.headerLabel.Text = "ActionProgressControl";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoEllipsis = true;
+            this.descriptionLabel.ForeColor = System.Drawing.Color.Gray;
+            this.descriptionLabel.Location = new System.Drawing.Point(3, 18);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(194, 18);
+            this.descriptionLabel.TabIndex = 2;
+            this.descriptionLabel.Text = "Description";
+            // 
             // ActionProgressControl
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(28)))));
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.headerLabel);
             this.Controls.Add(this.progressBar);
-            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
             this.Name = "ActionProgressControl";
@@ -65,5 +88,7 @@
 
         private System.Windows.Forms.Panel progressBar;
         private System.Windows.Forms.Panel progressBarValue;
+        private System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Label descriptionLabel;
     }
 }
--- Controls/ActionProgressControl.cs
+++ Controls/ActionProgressControl.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 
 namespace Trail.Controls {
     public partial class ActionProgressControl : UserControl {
+        public string HeaderText { get { return headerLabel.Text; } set { headerLabel.Text = value; } }
+        public string DescriptionText { get { return descriptionLabel.Text; } set { descriptionLabel.Text = value; } }
         public Color AccentColor { get { return progressBarValue.BackColor; } set { progressBarValue.BackColor = value; } }
         public int Progress {
             get { return (int)(100.0 * progressBarValue.Width / progressBar.Width); }
