f717e4c3729a7e6c048ca79a0f213d52bd451b7a
yishn <shen.yichuan@gmail.com>
2014-12-23 22:13:07 +0100

    Implement progress bar

--- Controls/ActionProgressControl.Designer.cs
+++ Controls/ActionProgressControl.Designer.cs
@@ -23,21 +23,47 @@
         /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
         /// </summary>
         private void InitializeComponent() {
+            this.progressBar = new System.Windows.Forms.Panel();
+            this.progressBarValue = new System.Windows.Forms.Panel();
+            this.progressBar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Controls.Add(this.progressBarValue);
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar.Location = new System.Drawing.Point(0, 39);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(200, 5);
+            this.progressBar.TabIndex = 0;
+            // 
+            // progressBarValue
+            // 
+            this.progressBarValue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.progressBarValue.Dock = System.Windows.Forms.DockStyle.Left;
+            this.progressBarValue.Location = new System.Drawing.Point(0, 0);
+            this.progressBarValue.Name = "progressBarValue";
+            this.progressBarValue.Size = new System.Drawing.Size(150, 5);
+            this.progressBarValue.TabIndex = 0;
             // 
             // ActionProgressControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(28)))));
+            this.Controls.Add(this.progressBar);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.Margin = new System.Windows.Forms.Padding(3, 3, 0, 3);
             this.Name = "ActionProgressControl";
             this.Size = new System.Drawing.Size(200, 44);
+            this.progressBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel progressBar;
+        private System.Windows.Forms.Panel progressBarValue;
     }
 }
--- Controls/ActionProgressControl.cs
+++ Controls/ActionProgressControl.cs
@@ -10,6 +10,12 @@ using System.Windows.Forms;
 
 namespace Trail.Controls {
     public partial class ActionProgressControl : UserControl {
+        public Color AccentColor { get { return progressBarValue.BackColor; } set { progressBarValue.BackColor = value; } }
+        public int Progress {
+            get { return (int)(100.0 * progressBarValue.Width / progressBar.Width); }
+            set { progressBarValue.Width = (int)(progressBar.Width * value / 100.0); }
+        }
+
         public ActionProgressControl() {
             InitializeComponent();
         }
