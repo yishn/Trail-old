2381c4de058395b981649d02157a391d50eec7c2
yishn <shen.yichuan@gmail.com>
2014-12-24 01:10:20 +0100

    Add ActionQueueList

---
+++ Modules/ActionQueueList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using Trail.Controls;
+using System.Collections.Specialized;
+
+namespace Trail.Modules {
+    public partial class ActionQueueList : UserControl {
+        public ObservableCollection<ActionProgressControl> ActionProgressControls { get; private set; }
+
+        public ActionQueueList() {
+            InitializeComponent();
+
+            ActionProgressControls = new ObservableCollection<ActionProgressControl>();
+
+            ActionProgressControls.CollectionChanged += ActionProgressControls_CollectionChanged;
+        }
+
+        private void ActionProgressControls_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e) {
+            if (e.Action == NotifyCollectionChangedAction.Add) {
+                foreach (ActionProgressControl c in e.NewItems) actionsList.Controls.Add(c);
+            }
+        }
+    }
+}
