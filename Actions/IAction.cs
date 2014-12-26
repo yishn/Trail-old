0837aaadfb76334d5da9e8819ccd1c5aada8e244
yishn <shen.yichuan@gmail.com>
2014-12-26 15:41:34 +0100

    Add IAction interface

---
+++ Actions/IAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Trail.Actions {
+    public interface IAction {
+        string HeaderText { get; }
+
+        void DoWork(IProgress<Tuple<int, string>> progress, CancellationToken token);
+    }
+}
