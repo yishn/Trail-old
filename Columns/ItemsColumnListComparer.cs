fb10b7775b852fa8d88c42ec9ea6374ff05e0474
yishn <shen.yichuan@gmail.com>
2014-12-27 18:13:13 +0100

    Remove unnecessary using directives

--- Columns/ItemsColumnListComparer.cs
+++ Columns/ItemsColumnListComparer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace Trail.Columns {
     public class ItemsColumnListComparer : IComparer {
--- MainForm.cs
+++ MainForm.cs
@@ -1,15 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Trail.Columns;
 using Trail.Controls;
