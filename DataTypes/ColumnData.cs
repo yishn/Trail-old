d054d76e21ccbfb77749313dfa7601c14dabf4dc
yishn <shen.yichuan@gmail.com>
2014-12-28 17:06:26 +0100

    Add DragDropData

--- DataTypes/ColumnData.cs
+++ DataTypes/ColumnData.cs
@@ -10,8 +10,6 @@ namespace Trail.DataTypes {
         public string ColumnType { get; set; }
         public string Path { get; set; }
 
-        public ColumnData() { }
-
         public ColumnData(string columnType, string path) {
             this.ColumnType = columnType;
             this.Path = path;
---
+++ DataTypes/DragDropData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trail.Columns;
+
+namespace Trail.DataTypes {
+    public class DragDropData {
+        public string SourceColumnType { get; set; }
+        public string DestinationColumnType { get; set; }
+        public ColumnListViewItem[] Items { get; set; }
+
+        public DragDropData(string srcColumnType, string destColumnType, ColumnListViewItem[] items) {
+            this.SourceColumnType = srcColumnType;
+            this.DestinationColumnType = destColumnType;
+            this.Items = items;
+        }
+    }
+}
