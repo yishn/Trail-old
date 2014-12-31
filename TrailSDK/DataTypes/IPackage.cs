0f93140eb4bf113e0e3d4ecc2dda0cb7527f0ad8
yishn <shen.yichuan@gmail.com>
2014-12-31 19:35:04 +0100

    Add Initialization method

--- TrailSDK/DataTypes/IPackage.cs
+++ TrailSDK/DataTypes/IPackage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Trail.DataTypes;
 
 namespace TrailSDK.DataTypes {
     public interface IPackage {
@@ -10,5 +11,7 @@ namespace TrailSDK.DataTypes {
         string Author { get; }
         string Website { get; }
         string Description { get; }
+
+        void Initialization(IHost host);
     }
 }
